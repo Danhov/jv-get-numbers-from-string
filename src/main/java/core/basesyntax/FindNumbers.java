7c975b9436029f3b1487a5d305c343c029d61342
Daniil Hovorun <hovorun.d@gmail.com>
2019-12-04 13:50:10 +0200

    hw:3.1-getAllNumbers at least it works, needs reowrk, I think

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class FindNumbers {
 
     /**
@@ -12,7 +14,35 @@ public class FindNumbers {
      * <p>Пример: если дана строка "data 48 call 9 read13 blank0a", то в массиве должны оказаться
      * числа 96, 18, 26 и 0.</p>
      */
+
+    //text.charAt(i) >= 48 && text.charAt(i) <= 57
     public int[] getAllNumbers(String text) {
-        return null;
+        int[] numbers = new int[text.length()];
+        int counter = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            StringBuilder sb = new StringBuilder();
+            /*if ( (text.charAt(i) >= 48 && text.charAt(i) <= 57)
+                    || (text.charAt(i) == 45 && text.charAt(i+1) >= 48
+                    && text.charAt(i+1) <= 57)) {*/
+            while (text.charAt(i) >= 48 && text.charAt(i) <= 57
+                    || (text.charAt(i) == 45 && text.charAt(i + 1) >= 48
+                    && text.charAt(i + 1) <= 57)) {
+                sb.append(text.charAt(i));
+                if (i + 1 >= text.length()) {
+                    break;
+                } else {
+                    i++;
+                }
+            }
+            if (!sb.toString().equals("")) {
+                numbers[counter] = Integer.parseInt(sb.toString()) * 2;
+                counter++;
+            }
+            //}
+
+        }
+        return Arrays.copyOf(numbers, counter);
     }
+
 }
